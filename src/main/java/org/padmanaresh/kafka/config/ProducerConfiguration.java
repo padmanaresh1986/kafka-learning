c163ee5f70c5a1eba2adfa91e39ac95a85fd7a9b
Padma Naresh <padmanaresh1986@gmail.com>
2020-04-06 22:11:33 +0530

    producer config

---
+++ src/main/java/org/padmanaresh/kafka/config/ProducerConfiguration.java
@@ -0,0 +1,21 @@
+package org.padmanaresh.kafka.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerConfiguration {
+// Below link is the documentation for producer configs
+// https://kafka.apache.org/documentation.html#producerconfigs
+
+    private static final String bootstrapServers = "127.0.0.1:9092";
+    public static Properties geProducerConfigs(){
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return properties;
+    }
+}
